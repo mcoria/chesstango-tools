6a176bc9e6cfc64e224a52a96ba020c38d5777bd
Mauricio Coria <coriamauricio@gmail.com>
2025-08-10 12:55:42 -0300

    Add `worker` module: implement `MatchWorkerMain` for RabbitMQ message consumption, update parent `pom.xml` with module inclusion, add dependencies in `worker/pom.xml`, and adjust Docker Compose for RabbitMQ integration.

--- worker/src/main/java/net/chesstango/tools/worker/MatchWorkerMain.java
+++ worker/src/main/java/net/chesstango/tools/worker/MatchWorkerMain.java
@@ -1,8 +1,9 @@
-package net.chesstango.tools.master;
+package net.chesstango.tools.worker;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 import org.apache.commons.cli.*;
 
 import java.nio.charset.StandardCharsets;
@@ -10,19 +11,24 @@ import java.nio.charset.StandardCharsets;
 /**
  * @author Mauricio Coria
  */
-public class MatchMasterMain {
+public class MatchWorkerMain {
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] args) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World3!";
-            channel.basicPublish("", QUEUE_NAME, null, message.getBytes(StandardCharsets.UTF_8));
-            System.out.println(" [x] Sent '" + message + "'");
-        }
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+        System.out.println(" [x] Done");
     }
 
     private static CommandLine parseArguments(String[] args) {

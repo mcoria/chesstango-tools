cf3ec70455c35919351c026ef0e4f88e5c5d99c2
Mauricio Coria <coriamauricio@gmail.com>
2025-09-28 02:58:21 -0300

    Migrate `EpdSearchBottomMoveCounterTest` and `EpdSearchBestMoveTest` to `epd-core.search` package:
    - Update package paths to `net.chesstango.epd.core.search`.
    - Adjust imports to reflect the new package structure.

--- epd-core/src/test/java/net/chesstango/epd/core/search/EpdSearchBestMoveTest.java
+++ epd-core/src/test/java/net/chesstango/epd/core/search/EpdSearchBestMoveTest.java
@@ -1,21 +1,19 @@
-package net.chesstango.tools;
+package net.chesstango.epd.core.search;
 
-import net.chesstango.board.Game;
 import net.chesstango.gardel.epd.EPD;
 import net.chesstango.gardel.epd.EPDDecoder;
 import net.chesstango.evaluation.Evaluator;
 import net.chesstango.evaluation.evaluators.EvaluatorImp04;
+import net.chesstango.reports.evaluation.EvaluationReport;
 import net.chesstango.search.Search;
-import net.chesstango.search.builders.BottomMoveCounterBuilder;
+import net.chesstango.search.builders.AlphaBetaBuilder;
 import net.chesstango.search.smart.features.debug.DebugNodeTrap;
 import net.chesstango.search.smart.features.debug.model.DebugNode;
 import net.chesstango.search.smart.features.debug.traps.ComposedTrap;
 import net.chesstango.search.smart.features.debug.traps.actions.PrintForUnitTest;
 import net.chesstango.search.smart.features.debug.traps.predicates.NodeByZobrist;
-import net.chesstango.epd.core.search.EpdSearch;
-import net.chesstango.epd.core.search.EpdSearchResult;
-import net.chesstango.reports.evaluation.EvaluationReport;
 import net.chesstango.reports.nodes.NodesReport;
+import net.chesstango.reports.pv.PrincipalVariationReport;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
@@ -23,14 +21,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 /**
  * @author Mauricio Coria
  */
-@Disabled
-public class EpdSearchBottomMoveCounterTest {
+public class EpdSearchBestMoveTest {
     private static final boolean PRINT_REPORT = false;
     private static EPDDecoder epdDecoder;
     private static EpdSearch epdSearch;
@@ -40,7 +37,8 @@ public class EpdSearchBottomMoveCounterTest {
     @BeforeAll
     public static void setup() {
         epdDecoder = new EPDDecoder();
-        epdSearch = new EpdSearch();
+        epdSearch = new EpdSearch()
+                .setEpdSearchResultBuilder(new EpdSearchResultBuildWithBestMove());
     }
 
     @AfterEach
@@ -57,6 +55,10 @@ public class EpdSearchBottomMoveCounterTest {
                     .withEvaluationsStatistics()
                     .withMoveResults(List.of(epdSearchResult.getSearchResult()))
                     .printReport(System.out);
+
+            new PrincipalVariationReport()
+                    .withMoveResults(List.of(epdSearchResult.getSearchResult()))
+                    .printReport(System.out);
         }
     }
 
@@ -65,8 +67,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("5rk1/1ppb3p/p1pb4/6q1/3P1p1r/2P1R2P/PP1BQ1P1/5RKN w - - bm Rg3; id \"WAC.003\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(40, Game.from(epd).getPossibleMoves().size());
-        assertEquals(39, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     // Evita entrar en loop
@@ -75,8 +76,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("r4q1k/p2bR1rp/2p2Q1N/5p2/5p2/2P5/PP3PPP/R5K1 w - - bm Rf7; id \"WAC.008\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(43, Game.from(epd).getPossibleMoves().size());
-        assertEquals(42, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -84,8 +84,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("4k1r1/2p3r1/1pR1p3/3pP2p/3P2qP/P4N2/1PQ4P/5R1K b - - bm Qxf3+; id \"WAC.012\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(26, Game.from(epd).getPossibleMoves().size());
-        assertEquals(25, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -93,8 +92,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(6);
         EPD epd = epdDecoder.readEdpLine("3b1rk1/1bq3pp/5pn1/1p2rN2/2p1p3/2P1B2Q/1PB2PPP/R2R2K1 w - - bm Rd7; id \"WAC.137\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(56, Game.from(epd).getPossibleMoves().size());
-        assertEquals(55, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -102,8 +100,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(1);
         EPD epd = epdDecoder.readEdpLine("r3r1k1/pp1n1ppp/2p5/4Pb2/2B2P2/B1P5/P5PP/R2R2K1 w - - bm e6; id \"WAC.072\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(36, Game.from(epd).getPossibleMoves().size());
-        assertEquals(35, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
 
@@ -112,8 +109,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("r1b1k2r/1pp1q2p/p1n3p1/3QPp2/8/1BP3B1/P5PP/3R1RK1 w kq - bm Bh4; id \"WAC.133\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(43, Game.from(epd).getPossibleMoves().size());
-        assertEquals(42, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
 
@@ -122,8 +118,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("6k1/p5p1/5p2/2P2Q2/3pN2p/3PbK1P/7P/6q1 b - - bm Qf1+; id \"WAC.111\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(27, Game.from(epd).getPossibleMoves().size());
-        assertEquals(26, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -131,8 +126,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("3q1rk1/4bp1p/1n2P2Q/3p1p2/6r1/Pp2R2N/1B4PP/7K w - - bm Ng5; id \"WAC.283\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(34, Game.from(epd).getPossibleMoves().size());
-        assertEquals(33, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
 
@@ -141,8 +135,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("1k1r4/pp1b1R2/3q2pp/4p3/2B5/4Q3/PPP2B2/2K5 b - - bm Qd1+; id \"BK.01\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(40, Game.from(epd).getPossibleMoves().size());
-        assertEquals(39, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -150,8 +143,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("rnbqkb1r/p3pppp/1p6/2ppP3/3N4/2P5/PPP1QPPP/R1B1KB1R w KQkq - bm e6; id \"BK.04\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(38, Game.from(epd).getPossibleMoves().size());
-        assertEquals(37, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -159,8 +151,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(2);
         EPD epd = epdDecoder.readEdpLine("r1b2rk1/2q1b1pp/p2ppn2/1p6/3QP3/1BN1B3/PPP3PP/R4RK1 w - - bm Nd5 a4; id \"BK.05\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(51, Game.from(epd).getPossibleMoves().size());
-        assertEquals(50, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -169,8 +160,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(3);
         EPD epd = epdDecoder.readEdpLine("2r3k1/pppR1pp1/4p3/4P1P1/5P2/1P4K1/P1P5/8 w - - bm g6; id \"BK.06\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(24, Game.from(epd).getPossibleMoves().size());
-        assertEquals(23, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -179,8 +169,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(3);
         EPD epd = epdDecoder.readEdpLine("r1bqk2r/pp2bppp/2p5/3pP3/P2Q1P2/2N1B3/1PP3PP/R4RK1 b kq - bm f6; id \"BK.23\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(35, Game.from(epd).getPossibleMoves().size());
-        assertEquals(34, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -188,8 +177,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(6);
         EPD epd = epdDecoder.readEdpLine("r2qnrnk/p2b2b1/1p1p2pp/2pPpp2/1PP1P3/PRNBB3/3QNPPP/5RK1 w - - bm f4; id \"BK.24\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(40, Game.from(epd).getPossibleMoves().size());
-        assertEquals(39, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -197,8 +185,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(1);
         EPD epd = epdDecoder.readEdpLine("1Q6/6kp/6p1/8/1P2q3/5R1P/5PK1/4r3 w - - bm Qb8-f8+; ce +M1; pv Qb8-f8+; id \"10390\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(21, Game.from(epd).getPossibleMoves().size());
-        assertEquals(20, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -206,17 +193,15 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(1);
         EPD epd = epdDecoder.readEdpLine("4R3/5pk1/5r1p/1p2Q3/8/6P1/6KP/1q6 b - - bm Qb1-f1+; id \"10390\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(21, Game.from(epd).getPossibleMoves().size());
-        assertEquals(20, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
     public void test_40H_9482() {
-        epdSearch.setDepth(1);//El JM se encuentra a un ply
+        epdSearch.setDepth(30);//El JM se encuentra a un ply
         EPD epd = epdDecoder.readEdpLine("1Q4rr/1p1bkp2/pP6/2p1pP2/3nP1Bp/2P1q2P/7K/3R1R2 w - - bm f5-f6+; ce +M1; pv f5-f6+; id \"9482\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(32, Game.from(epd).getPossibleMoves().size());
-        assertEquals(31, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -224,8 +209,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(3);
         EPD epd = epdDecoder.readEdpLine("1R1nk2r/4q1p1/5pP1/3QpP1p/P6P/5P2/5BK1/2r5 w k - bm Rb8xd8+; ce +M2; pv Rb8xd8+ Qe7xd8 Qd5-f7+; id \"079\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(44, Game.from(epd).getPossibleMoves().size());
-        assertEquals(43, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -233,8 +217,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("1R3b1k/2p3pp/4qr2/Q7/3p2P1/3P3K/6NP/8 b - - bm Rf6-f3+; ce -M3; pv Rf6-f3+ Kh3-h4 Qe6-h6+ Qa5-h5 g7-g5+; id \"2857\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(36, Game.from(epd).getPossibleMoves().size());
-        assertEquals(35, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -242,8 +225,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(9);
         EPD epd = epdDecoder.readEdpLine("1B1bk1nr/5pp1/rNR5/p3Pp1p/1p2pP2/1P5P/1PP3P1/2KR4 w k - bm Rd1xd8+; ce +M5; pv Rd1xd8+ Ke8xd8 Rc6-c8+ Kd8-e7 Bb8-d6+ Ke7-e6 Rc8-e8+ Ng8-e7 Re8xe7+; id \"5712\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(38, Game.from(epd).getPossibleMoves().size());
-        assertEquals(37, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -251,8 +233,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("1k2r3/1pp5/4B3/1P3Q2/3q1Pp1/3n2Pp/3p3P/5R1K b - - bm Qd4-f2; ce -M3; pv Qd4-f2 Rf1-g1 Qf2-f3+ Rg1-g2 d2-d1Q+; id \"2462\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(46, Game.from(epd).getPossibleMoves().size());
-        assertEquals(45, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -260,8 +241,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("5r1k/3P3p/3N2pP/3Q1pP1/1p3q2/4b3/1PP5/1K2R3 w - - bm Qd5-f7; id \"2462\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(46, Game.from(epd).getPossibleMoves().size());
-        assertEquals(45, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -269,8 +249,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("2r3k1/p4p2/3Rp2p/1p2P1pK/8/1P4P1/P3Q2P/1q6 b - - bm Qb1-g6+; ce -M3; pv Qb1-g6+ Kh5-g4 Qg6-f5+ Kg4-h5 Qf5-h3+; id \"1445\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(38, Game.from(epd).getPossibleMoves().size());
-        assertEquals(37, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -278,8 +257,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("3k4/p2r4/1pR4p/4Q3/8/5P2/q5P1/6K1 w - - bm Qe5-f6+; ce +M3; pv Qe5-f6+ Rd7-e7 Qf6-f8+ Re7-e8 Qf8-d6+; id \"10021\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(46, Game.from(epd).getPossibleMoves().size());
-        assertEquals(44, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -287,8 +265,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(1);
         EPD epd = epdDecoder.readEdpLine("6kr/1p3ppp/p1q2b2/2Bp1N2/3P4/6Q1/PP3PPP/4R1K1 w - - bm Nf5-h6+; ce +M1; pv Nf5-h6+; id \"022\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(52, Game.from(epd).getPossibleMoves().size());
-        assertEquals(51, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -296,8 +273,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(6);
         EPD epd = epdDecoder.readEdpLine("5r2/ppbqn2k/7B/2p1p2p/P1NpP1P1/3P4/1PP2r2/R1Q1K1R1 b Q - bm Qd7xg4; ce -M3; pv Qd7xg4 Rg1xg4 Rf2-f1+ Ke1-e2 Rf8-f2+; id \"10255\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(54, Game.from(epd).getPossibleMoves().size());
-        assertEquals(53, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -305,8 +281,7 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("kbK5/pp6/1P6/8/8/8/R7/8 w - - bm Ra2-a6; ce +M2; pv Ra2-a6 b7xa6 b6-b7+; id \"1027\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(16, Game.from(epd).getPossibleMoves().size());
-        assertEquals(15, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     @Test
@@ -314,18 +289,31 @@ public class EpdSearchBottomMoveCounterTest {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("6k1/p1pp2pp/2p5/8/3P3q/3bP3/PP3rPP/R1K1Q2R b - - bm Rf2-c2+; ce -M3; pv Rf2-c2+ Kc1-d1 Qh4-g4+ Qe1-e2 Qg4xe2+; id \"3243\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(47, Game.from(epd).getPossibleMoves().size());
-        assertEquals(46, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
+    @Test
+    public void test_40H_1043() {
+        epdSearch.setDepth(5);
+        EPD epd = epdDecoder.readEdpLine("r1b1Rbkr/pp4pp/2p3n1/3p2BB/8/2N2R2/PPP2PPP/2K5 w - - bm Re8xf8+; ce +M2; pv Re8xf8+ Ng6xf8 Bh5-f7+; id \"1043\";");
+        epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
+        assertTrue(epdSearchResult.isSearchSuccess());
+    }
 
     @Test
     public void test_sbd_058() {
         epdSearch.setDepth(5);
         EPD epd = epdDecoder.readEdpLine("r1bqk2r/2ppbppp/p1n2n2/1p2p3/4P3/1B3N2/PPPPQPPP/RNB2RK1 b kq - bm O-O; id \"sbd.058\";");
         epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
-        assertEquals(30, Game.from(epd).getPossibleMoves().size());
-        assertEquals(29, epdSearchResult.getBottomMoveCounter());
+        assertTrue(epdSearchResult.isSearchSuccess());
+    }
+
+    @Test
+    public void test_kaufman_pos3() {
+        epdSearch.setDepth(5);
+        EPD epd = epdDecoder.readEdpLine("3r2k1/1p3ppp/2pq4/p1n5/P6P/1P6/1PB2QP1/1K2R3 w - - am Rd1; id \"position 03\";");
+        epdSearchResult = epdSearch.run(buildSearchMove(new EvaluatorImp04()), epd);
+        assertTrue(epdSearchResult.isSearchSuccess());
     }
 
     private void trapNodeByZobristAndPrintForUT() {
@@ -339,18 +327,23 @@ public class EpdSearchBottomMoveCounterTest {
 
 
     private static Search buildSearchMove(Evaluator evaluator) {
-        BottomMoveCounterBuilder builder = new BottomMoveCounterBuilder()
+        AlphaBetaBuilder builder = new AlphaBetaBuilder()
                 .withGameEvaluator(evaluator)
                 .withGameEvaluatorCache()
 
+                //.withExtensionCheckResolver()
                 .withQuiescence()
 
+                //.withTriangularPV()
                 .withTranspositionTable()
 
                 .withTranspositionMoveSorter()
                 .withKillerMoveSorter()
                 .withRecaptureSorter()
                 .withMvvLvaSorter()
+
+                .withAspirationWindows()
+                .withIterativeDeepening()
 
                 //.withStopProcessingCatch()
                 //.withPrintChain()
